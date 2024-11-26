67e0236349c3495c73a6fcff31a5944fd5019802
Allan <allanglopez1312@gmail.com>
2024-11-26 12:43:03 +0100

    En el Main, ponemos todos los reader e invocamos la biblioteca.

--- src/org/ies/library/Main.java
+++ src/org/ies/library/Main.java
@@ -1,7 +1,21 @@
 package org.ies.library;
 
+import org.ies.library.components.AuthorReader;
+import org.ies.library.components.BookReader;
+import org.ies.library.components.LibraryReader;
+import org.ies.library.model.Biblioteca;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AuthorReader authorReader = new AuthorReader(scanner);
+        BookReader bookReader = new BookReader(scanner);
+        LibraryReader libraryReader = new LibraryReader(scanner);
 
+        Biblioteca biblioteca = libraryReader.read();
+
+        System.out.println(biblioteca);
     }
 }
